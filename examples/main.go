ee64cfb40b8cf1876a76787bc22fbda7a3f2d044
Sendya <18x@loacg.com>
2025-12-01 22:05:44 +0800

    fix: update proxy selector to use 'once' and clean up peer context code

--- examples/main.go
+++ examples/main.go
@@ -9,30 +9,36 @@ import (
 
 	"github.com/omalloc/proxy"
 	"github.com/omalloc/proxy/selector"
-	"github.com/omalloc/proxy/selector/aio"
+	"github.com/omalloc/proxy/selector/once"
 )
 
 func main() {
 	// use default proxy client
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Via", "self-node-test")
 		w.WriteHeader(http.StatusOK)
 	}))
 	u, _ := url.Parse(ts.URL)
 
 	proxyClient := proxy.New(
-		proxy.WithSelector(aio.New()),
+		proxy.WithSelector(once.New()),
 		proxy.WithInitialNodes([]selector.Node{
 			selector.NewNode(u.Scheme, u.Host, nil),
 		}),
 	)
 
 	req, _ := http.NewRequest(http.MethodGet, ts.URL, nil)
-	req = req.WithContext(selector.NewPeerContext(req.Context(), &selector.Peer{selector.NewNode("http", "127.0.0.1:8282", nil)}))
+	// dynamicly set peer info in context
+	//
+	// req = req.WithContext(selector.NewPeerContext(req.Context(),
+	// 	selector.NewPeer(selector.NewNode("http", "127.0.0.1:8282", nil)),
+	// ))
 
 	resp, err := proxyClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
+
 	info, _ := httputil.DumpResponse(resp, false)
 	log.Printf("Response Info : \n%v", string(info))
 }
